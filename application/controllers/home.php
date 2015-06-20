7bc7cda5935072fc7dd2c6ac128d9690b021f74b
faizanayubi <indianayubi@gmail.com>
2015-06-20 21:43:25 +0530

    Added Login Method

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -6,12 +6,30 @@
  * @author Faizan Ayubi
  */
 use Shared\Controller as Controller;
+use Framework\RequestMethods as RequestMethods;
 
 class Home extends Controller {
 
     public function index() {
         $this->seo(array("title" => "SwiftMarketing", "view" => $this->getLayoutView()));
         $view = $this->getActionView();
+    }
+    
+    public function login() {
+        $this->seo(array("title" => "Login", "view" => $this->getLayoutView()));
+        $view = $this->getActionView();
+        
+        if(RequestMethods::post("action") == "login"){
+            $email = RequestMethods::post("email");
+            $password = RequestMethods::post("password");
+            $user = User::first(array("email = ?" => $email, "password = ?" => $password));
+            if ($user) {
+                $this->user = $user;
+                self::redirect("/app");
+            } else {
+                $view->set("success", "Wrong username or password");
+            }
+        }
     }
 
 }
