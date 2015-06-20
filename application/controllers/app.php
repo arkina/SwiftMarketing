c28e4c0218259d60ab21fb2d97d9606b875182fc
faizanayubi <indianayubi@gmail.com>
2015-06-20 21:55:05 +0530

    Changed App Layout

--- application/controllers/app.php
+++ application/controllers/app.php
@@ -5,5 +5,21 @@
  * @author Faizan Ayubi
  */
 class App extends Home{
-    //put your code here
+    
+    /**
+     * @before _secure, changeLayout
+     */
+    public function index() {
+        $this->seo(array("title" => "SwiftMarketing", "view" => $this->getLayoutView()));
+        $view = $this->getActionView();
+    }
+    
+    /**
+     * Changes the Standard Layout
+     */
+    public function changeLayout() {
+        $this->defaultLayout = "layouts/app";
+        $this->setLayout();
+    }
+    
 }
