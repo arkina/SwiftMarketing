e98b6e0a11a883eacfa4605540994fa4b8703e94
faizanayubi <indianayubi@gmail.com>
2015-06-21 06:22:22 +0530

    Added Stats model

---
+++ application/models/stat.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * The Stat Model
+ *
+ * @author Faizan Ayubi
+ */
+class Stat extends Shared\Model {
+
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 255
+     * 
+     * @validate required
+     * @label name
+     */
+    protected $_property;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_property_id;
+
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_viewed;
+
+    public static function log($property, $property_id) {
+        $date = strftime("%Y-%m-%d", strtotime('now'));
+        $stat = Stat::first(array(
+            "property = ?" => $property,
+            "property_id = ?" => $property_id,
+            "created LIKE ?" => $date
+        ));
+        if($stat){
+            $stat->viewed += 1;
+        } else {
+            $stat = new Stat(array(
+                "property" => $property,
+                "property_id" => $property_id,
+                "viewed" => "1"
+            ));
+        }
+        $stat->save();
+        return true;
+    }
+}
